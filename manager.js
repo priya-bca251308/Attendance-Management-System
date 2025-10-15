61188c6f8a14da9a011b8c3c07289e4258631aa7
Priya Dharshini <priyadarshini251205@gmail.com>
2025-10-15 06:08:35 +0530

    Add files via upload

---
+++ manager.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const managerSchema =new mongoose.Schema({
+  name:{type: String,
+    required: true},
+  email: {type: String,
+     required:true },
+  password:{type: String,
+    required: true },
+  role: { type:String, 
+    default:'manager'}
+});
+
+module.exports = mongoose.model("Manager", managerSchema);
